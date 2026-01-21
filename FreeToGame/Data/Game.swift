39078dee4e7c12b04bd867173b8b3575a7ec5add
Mansour Boutarbouch Mhaimeur <theboom.kajas@gmail.com>
2026-01-21 17:50:55 +0100

    Added all struct and properties need to deserialize the JSON

--- FreeToGame/Data/Game.swift
+++ FreeToGame/Data/Game.swift
@@ -12,4 +12,36 @@ struct Game: Codable {
     let id: Int
     let title: String
     let thumbnail: String
+    let shortDescription: String
+    let gameURL: String
+    let genre: String
+    let platform: String
+    let publisher: String
+    let developer: String
+    let releaseDate: String
+    let profileURL: String
+    let description: String?
+    let systemRequirments: SystemRequirments?
+    let screenshots: [Screenshot]?
+    
+    enum CodingKeys: String, CodingKey {
+        case shortDescription = "short_description"
+        case gameURL = "game_url"
+        case releaseDate = "release_date"
+        case profileURL = "freetogame_profile_url"
+        case systemRequirments = "minimum_system_requirements"
+        case id, title, thumbnail, genre, platform, publisher, developer, description, screenshots
+    }
+}
+
+struct SystemRequirments: Codable {
+    let os: String
+    let processor: String
+    let memory: String
+    let graphics: String
+    let storage: String
+}
+
+struct Screenshot: Codable {
+    let image: String
 }
