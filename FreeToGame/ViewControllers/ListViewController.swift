d998bf1eef5463fdf17ea3eee9adccf79f681997
Mansour Boutarbouch Mhaimeur <theboom.kajas@gmail.com>
2026-01-26 16:37:31 +0100

    Added a SearchBar for filtering

--- FreeToGame/ViewControllers/ListViewController.swift
+++ FreeToGame/ViewControllers/ListViewController.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 
-class ListViewController: UIViewController, UITableViewDataSource {
+class ListViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
     
     
     @IBOutlet weak var tableView: UITableView!
     
-    var gameList: [Game] = []
+    var originalGameList: [Game] = []
+    var filteredGameList: [Game] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +21,13 @@ class ListViewController: UIViewController, UITableViewDataSource {
         
         tableView.dataSource = self
         
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.delegate = self
+        navigationItem.searchController = searchController
+        
         Task {
-            gameList = await GameProvider.getGameList()
+            originalGameList = await GameProvider.getGameList()
+            filteredGameList = originalGameList
             
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -30,20 +36,37 @@ class ListViewController: UIViewController, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return gameList.count
+        return filteredGameList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Game Cell", for: indexPath) as! GameViewCell
-        let game = gameList[indexPath.row]
+        let game = filteredGameList[indexPath.row]
         cell.configure(with: game)
         return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filteredGameList = originalGameList
+        } else {
+            filteredGameList = originalGameList.filter { game in
+                game.title.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        filteredGameList = originalGameList
+        tableView.reloadData()
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let detailViewController = segue.destination as! DetailViewController
         let indexPath = tableView.indexPathForSelectedRow!
-        let game = gameList[indexPath.row]
+        let game = filteredGameList[indexPath.row]
         detailViewController.game = game
         tableView.deselectRow(at: indexPath, animated: true)
     }
